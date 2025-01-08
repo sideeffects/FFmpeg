f3db8ad63f195dcc1cf4feb33969d4df6616d378
jmather-sesi <54645798+jmather-sesi@users.noreply.github.com>
2025-01-08 17:05:08 -0500

    Allow sesi_binary_repather to repath libraries with @rpath

--- sesi_binary_repather.py
+++ sesi_binary_repather.py
@@ -7,10 +7,10 @@ def main(localdir, binary):
     # Binaries produced by the FFmpeg build system contain absolute paths to the 
     # built libaries. This tool rewrites them to work within Houdini.
     otool_output = subprocess.check_output(['otool', '-L', binary]).decode('utf-8')
-    libraries_to_repath = re.findall(localdir + '/lib/([^/]+\.dylib)', otool_output)
+    libraries_to_repath = re.findall(localdir + '/([^/]+\.dylib)', otool_output)
 
     for library_to_repath in libraries_to_repath:
-        change_from = localdir + '/lib/' + library_to_repath
+        change_from = localdir + '/' + library_to_repath
         change_to   = '@loader_path/../../Libraries/' + library_to_repath
 
         cmd = ['install_name_tool', '-change', change_from, change_to, binary]
